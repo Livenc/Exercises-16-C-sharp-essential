8486a3ee061bffa29da8439ef300c2d05ae49287
Artem Alimov <artemalimov333@gmail.com>
2023-04-04 21:58:02 +0300

    Add project files.

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        public class Block
+        {
+            public int AB { get; set; }
+            public int BC { get; set; }
+            public int CD { get; set; }
+            public int DA { get; set; }
+           public Block(int ab, int bc, int cd, int da)
+            {
+                AB = ab;
+                BC = bc;
+                CD = cd;
+                DA = da;
+            }
+            public override bool Equals(object? obj)
+            {
+                if (obj == null || this.GetType() != obj.GetType())
+                {
+                    return false;
+                }
+                Block block = (Block)obj;
+                return(AB==block.AB)&&(BC == block.BC) &&(CD ==block.CD)&&(DA ==block.DA);
+            }
+            public override int GetHashCode()
+            {
+                return AB^ BC ^ CD ^ DA;
+            }
+            public override string ToString()
+            {
+                return $"{AB} - {BC}  -  {CD} - {DA}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Block block = new(1, 2, 3, 4);
+            Block block1 = new(1, 2, 3, 4);
+
+            Console.WriteLine(block.Equals(block1));
+            Console.WriteLine(block.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise_3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http.Headers;
+
+namespace Exercise_3
+{
+    public class House
+    {
+        private string _address ;
+        private int _numRooms ;
+        
+
+        public House(string address, int numRooms)
+        {
+            _address = address;
+            _numRooms = numRooms;
+        }
+
+        public House()
+        {
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public int NumRooms
+        {
+            get { return _numRooms; }
+            set { _numRooms = value; }
+        }
+        public House Clone()
+        {
+            return (House)this.MemberwiseClone();
+        }
+
+        public House DeepClone()
+        {
+            return this;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House("dfs",1);
+            Console.WriteLine(house.Address + " " + house.NumRooms);
+            House clone1 = house.Clone();
+            Console.WriteLine(clone1.Address + " " + clone1.NumRooms);
+            //clone1.Address = "Akil";
+            //Console.WriteLine(clone1.Address + " " + clone1.NumRooms);
+            //Console.WriteLine(house.Address + " " + house.NumRooms);
+
+            House clone2 = house.DeepClone();
+            Console.WriteLine(clone2.Address + " " + clone2.NumRooms);
+            clone2.Address = "Liven";
+            Console.WriteLine(house.Address + " " + house.NumRooms);
+            Console.WriteLine(clone2.Address + " " + clone2.NumRooms);
+            Console.ReadKey();
+
+        }
+    }
+}
